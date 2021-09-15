d593da5a7d46fe649969c365b9fff083ffbcac61
Lukas <lnowakowski.ln@gmail.com>
2021-09-15 17:10:32 +0200

    update commit

--- Models/Character.cs
+++ Models/Character.cs
@@ -3,7 +3,7 @@ namespace dotnet_rpg.Models
     public class Character
     {
         
-                public int Id { get; set; }
+        public int Id { get; set; }
         
         public string Name { get; set; } = "Frodo"; 
 
