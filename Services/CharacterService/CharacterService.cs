51b74e3d5d2aa45c0c1a6bca4e9a11652ed55c2a
Lukas <lnowakowski.ln@gmail.com>
2021-09-16 16:34:08 +0200

    POST impl to db

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -8,7 +8,8 @@ using dotnet_rpg.Dtos.Character;
 using AutoMapper;
 using dotnet_rpg.Models;
 using dotnet_rpg.Services;
-
+using Microsoft.EntityFrameworkCore;
+using dotnet_rpg.Data;
 
 
 namespace dotnet_rpg.Services.CharacterService
@@ -21,8 +22,11 @@ namespace dotnet_rpg.Services.CharacterService
             new Character {Id = 1, Name = "Sam"}
         };
         private readonly IMapper _mapper;
+        
+        private readonly DataContext _context;
 
-        public CharacterService(IMapper mapper){
+        public CharacterService(IMapper mapper, DataContext context){
+            _context = context;
             _mapper = mapper;
         }
 
@@ -30,9 +34,10 @@ namespace dotnet_rpg.Services.CharacterService
     {
         var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
         Character character = _mapper.Map<Character>(newCharacter);
-        character.Id = characters.Max(c => c.Id) + 1;
-        characters.Add(character);
-        serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();//adding srvcerespns data to character
+
+        _context.Characters.Add(character);
+        await _context.SaveChangesAsync();
+        serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();//adding srvcerespns data to character
         return serviceResponse; //returns ServiceResponse object  as response
     }
 
@@ -59,14 +64,16 @@ namespace dotnet_rpg.Services.CharacterService
     public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
     {
         var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-        serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+        var dbCharacters = await _context.Characters.ToListAsync();//to get all characters from database
+        serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
         return serviceResponse;
     }
 
     public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
     {
         var serviceResponse = new ServiceResponse<GetCharacterDto>();
-        serviceResponse.Data = _mapper.Map<GetCharacterDto>(characters.FirstOrDefault(c =>c.Id == id)); //serviceResponse.Data is actual result
+        var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+        serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter); //serviceResponse.Data is actual result
         return serviceResponse;
 
     }
