0f77ca9002d2e23f8586235d9afbd7e3b4d1d83c
Lukas <lnowakowski.ln@gmail.com>
2021-09-16 14:33:16 +0200

    setting dbconnection

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using dotnet_rpg.Models;
+using dotnet_rpg.Data;
+
+
+
+namespace dotnet_rpg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Character> Characters {get; set;} //enables to query and save rpg characters
+    }
+}
