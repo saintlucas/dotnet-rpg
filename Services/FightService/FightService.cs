92cb7e0bce42601d6c8afaef7ab4bc052bba6ebb
Lukas <lnowakowski.ln@gmail.com>
2021-09-28 14:39:06 +0200

    Authomatic fight added

--- Services/FightService/FightService.cs
+++ Services/FightService/FightService.cs
@@ -31,13 +31,8 @@ namespace dotnet_rpg.Services.FightService
 
                 var opponent = await _context.Characters
                     .FirstOrDefaultAsync(c => c.Id == request.OpponentId);
+                int damage = DoWeaponAttack(attacker, opponent);
 
-                int damage = attacker.Weapon.Damage + (new Random().Next(attacker.Strenght));
-                damage -= new Random().Next(opponent.Defence);
-
-                if(damage > 0)
-                    opponent.Hitpoints -= damage;
-                
                 if (opponent.Hitpoints <= 0)
                     response.Message = $"{opponent.Name} has been defeated";
 
@@ -51,11 +46,142 @@ namespace dotnet_rpg.Services.FightService
                     OpponentHP = opponent.Hitpoints,
                     Damage = damage
                 };
-            }   
+            }
             catch (Exception ex)
             {
                 response.Success = false;
                 response.Message = ex.Message;  
+            }
+            return response;
+        }
+
+        private static int DoWeaponAttack(Character attacker, Character opponent)
+        {
+            int damage = attacker.Weapon.Damage + (new Random().Next(attacker.Strenght));
+            damage -= new Random().Next(opponent.Defence);
+
+            if (damage > 0)
+                opponent.Hitpoints -= damage;
+            return damage;
+        }
+
+        public async Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request)
+        {
+
+            var response = new ServiceResponse<AttackResultDto>();
+            try
+            {
+                var attacker = await _context.Characters
+                    .Include(c => c.Skills)
+                    .FirstOrDefaultAsync(c => c.Id == request.AttackerId);
+
+                var opponent = await _context.Characters
+                    .FirstOrDefaultAsync(c => c.Id == request.OpponentId);
+
+                var skill = attacker.Skills.FirstOrDefault(s => s.Id == request.SkillId);
+                if (skill is null)
+                {
+                    response.Success = false;
+                    response.Message = $"{attacker.Name} doesn't know this skill.";
+                    return response;
+                }
+
+                int damage = DoSkillAttack(attacker, opponent, skill);
+
+                if (opponent.Hitpoints <= 0)
+                    response.Message = $"{opponent.Name} has been defeated";
+
+                await _context.SaveChangesAsync();
+
+                response.Data = new AttackResultDto
+                {
+                    Attacker = attacker.Name,
+                    AttackerHP = attacker.Hitpoints,
+                    Opponent = opponent.Name,
+                    OpponentHP = opponent.Hitpoints,
+                    Damage = damage
+                };
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            return response;
+        }
+
+        private static int DoSkillAttack(Character attacker, Character opponent, Skill skill)
+        {
+            int damage = skill.Damage + (new Random().Next(attacker.Inteligence));
+            damage -= new Random().Next(opponent.Defence);
+
+            if (damage > 0)
+                opponent.Hitpoints -= damage;
+            return damage;
+        }
+
+        public async Task<ServiceResponse<FightResultDto>> Fight(FightRequestDto request)
+        {
+            var response = new ServiceResponse<FightResultDto>
+            { 
+                Data = new FightResultDto()
+            };
+
+            try
+            {
+                var characters = await _context.Characters
+                    .Include(c => c.Weapon)
+                    .Include(c => c.Skills)
+                    .Where(c => request.CharacterIds.Contains(c.Id)).ToListAsync();
+                bool defeated = false;
+                while (!defeated)
+                {
+                    foreach (var attacker in characters)
+                    {
+                        var opponents = characters.Where(c => c.Id != attacker.Id).ToList();
+                        var opponent = opponents[new Random().Next(opponents.Count)];
+
+                        int damage = 0;
+                        string attackedUsed = string.Empty;
+
+                        bool useWeapon = new Random().Next(2) == 0;
+                        if (useWeapon)
+                        {
+                            attackedUsed = attacker.Weapon.Name;
+                            damage = DoWeaponAttack(attacker, opponent);
+                        }
+                        else
+                        {
+                            var skill = attacker.Skills[new Random().Next(attacker.Skills.Count)];
+                            attackedUsed = skill.Name;
+                            damage = DoSkillAttack(attacker, opponent, skill);
+                        }
+
+                        response.Data.Log
+                            .Add($"{attacker.Name} attacks {opponent.Name} using {attackedUsed} with {(damage >= 0 ? damage : 0)} damage");
+                        if (opponent.Hitpoints <= 0)
+                        {
+                            defeated = true;
+                            attacker.Victories++;
+                            opponent.Defeats++;
+                            response.Data.Log.Add($"{opponent.Name} has been defeated!");
+                            response.Data.Log.Add($"{attacker.Name} wins with {attacker.Hitpoints} HP left!");
+                            break;
+                        }
+                    }
+                }
+
+                characters.ForEach(c =>
+                {
+                    c.Fights++;
+                    c.Hitpoints = 100;
+                });
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
             }
             return response;
         }
